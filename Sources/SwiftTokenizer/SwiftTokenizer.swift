46c32c88c8d1be070dbfd9530a62711489bc1376
Tsungyu Yu <ytyubox@gmail.com>
2021-03-05 10:58:00 +0800

    can seperate Mandurin text into token

--- Sources/SwiftTokenizer/SwiftTokenizer.swift
+++ Sources/SwiftTokenizer/SwiftTokenizer.swift
@@ -1,3 +1,34 @@
+import Foundation
 struct SwiftTokenizer {
-    var text = "Hello, World!"
+    let sourceString:String
+    
+    func wordTokens() -> [String] {
+        let cfString = sourceString as CFString
+        let locale = CFLocaleCopyCurrent()
+        let range = CFRangeMake(0, CFStringGetLength(cfString))
+        let tokenizer = CFStringTokenizerCreate(
+            kCFAllocatorDefault,
+            cfString, range,
+                kCFStringTokenizerUnitWordBoundary
+//                kCFStringTokenizerUnitSentence
+            ,
+            locale)
+        
+        var tokens = [String]()
+        var tokenType = CFStringTokenizerAdvanceToNextToken(tokenizer)
+        dump(tokenType)
+        while true {
+            if tokenType.isEmpty {break}
+            let range = CFStringTokenizerGetCurrentTokenRange(tokenizer)
+            let token = CFStringCreateWithSubstring(kCFAllocatorDefault, cfString, range)
+            tokens.append(token! as String)
+            tokenType = CFStringTokenizerAdvanceToNextToken(tokenizer)
+        }
+        
+        return tokens
+    }
+    
+    func separatedStringWithSeparator(separator: String) -> String {
+        return (self.wordTokens() as NSArray).componentsJoined(by: separator)
+    }
 }
